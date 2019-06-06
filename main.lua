eacbd48f5a436cb15dc5b4e5552674833925111e
Mouhamadou Gaye <mouhagaye2015@gmail.com>
2019-06-06 14:45:14 +0000

    pong-1

--- main.lua
+++ main.lua
@@ -1,20 +1,33 @@
+love.graphics.setDefaultFilter('nearest','nearest')
+
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT,{
+    push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT, WINDOW_WIDTH,WINDOW_HEIGHT,{
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw()
+
+    push:apply('start')
+
     love.graphics.printf(
-        'Hello pong',
-        0,
-        WINDOW_HEIGHT/2 - 6,
-        WINDOW_WIDTH,
-        'center'
+        'Hello pong',0,VIRTUAL_HEIGHT/2 - 6, VIRTUAL_WIDTH,'center'
     )
+    push:apply('end')
 end
